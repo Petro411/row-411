b1a2c31c17c558f5756dd885e70024c08b29b719
Hasnain Alam <102312863+Devhasnain@users.noreply.github.com>
2025-07-04 00:52:56 +0500

    Update home page title

--- src/config/Label.ts
+++ src/config/Label.ts
@@ -1,6 +1,6 @@
 const Label = {
     // Home Page 
-    YourMineralOwners:"Mineral Owner Contact HUBN",
+    YourMineralOwners:"Mineral Owner Contact HUB",
     SimplifiesLandAcquisition:"Petro411 simplifies land acquisition by providing accurate, secure contact information for mineral owners nationwide — built by industry experts for the oil and gas community.",
     SearchMineralOwners:"Search mineral owners",
     FindMineralOwners:"Find mineral owners fast with accurate contact details to support your land projects.",
