34b126b553810943d4219d6c1edacab6fa229b84
devhasnain <hasnainalam1166@gmail.com>
2025-10-13 00:50:28 +0500

    #added all brands

--- src/branding/index.ts
+++ src/branding/index.ts
@@ -1,5 +1,8 @@
+import Pipeline411 from "./pipeline411";
+import Surface411 from "./surface411";
 import Petro411 from "./petro411";
 import Digg411 from "./digg411";
+import Row411 from "./row411";
 
 
 const BRAND: any = "petro"; // can be from env
@@ -12,6 +15,15 @@ switch (BRAND) {
         break;
     case "digg":
         BrandConfig = Digg411;
+        break;
+    case "pipeline":
+        BrandConfig = Pipeline411;
+        break;
+    case "row":
+        BrandConfig = Row411;
+        break;
+    case "surface":
+        BrandConfig = Surface411;
         break;
     case "petro":
         BrandConfig = Petro411;
---
+++ src/branding/pipeline411/index.ts
@@ -0,0 +1,10 @@
+import labels from "./labels";
+import colors from "./colors";
+
+
+const Digg411 =  {
+    labels,
+    colors
+};
+
+export default Digg411
